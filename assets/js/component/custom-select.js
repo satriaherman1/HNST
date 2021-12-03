827f082709097eacf8905a422db9a1e9f0ddcd92
satriaherman <muhammadsatriaherman@gmail.com>
2021-12-03 09:48:09 +0700

    init git

---
+++ assets/js/component/custom-select.js
@@ -0,0 +1,42 @@
+const customSelectField = document.querySelectorAll('.custom-select-field')
+const allSelect = document.querySelectorAll('.custom-select')
+
+const optionListClick = (opt, index) => {
+    const currentSelect = customSelectField[index].querySelector('select');
+    currentSelect.value = opt.textContent;
+    currentSelect.onchange();
+    opt.parentElement.classList.add('d-none');
+}
+
+const selectClick = (select) => {
+    const currentCustomSelectList = select.parentElement.querySelector('.custom-select-list')
+    currentCustomSelectList.classList.toggle('d-none')
+}
+
+const changeContentTarget = (target, self = false) => {
+    const elementTarget = document.querySelector(target)
+    const content = self.value.charAt(0)
+    elementTarget.innerHTML = content
+}
+
+window.onload = function(){
+    allSelect.forEach((select) => {
+        select.innerHTML += `<div class="custom-select-arrow"></div>`
+    })
+}
+
+customSelectField.forEach((field , index) => {
+    const customSelect = field.querySelector('.custom-select')
+    const customSelectList = field.querySelector('.custom-select-list')
+
+    const opt = customSelect.querySelectorAll('option')
+    opt.forEach((el) => {
+        if(!el.disabled){
+            customSelectList.innerHTML += `<li class="custom-option-list" 
+                                            onclick="optionListClick(this , ${index})">${el.value}</li>`
+        }
+    })
+
+
+})
+
---
+++ assets/js/main.js
@@ -0,0 +1,26 @@
+$('#slickReels').slick({
+  centerMode: true,
+  centerPadding: '10px',
+  slidesToShow: 3,
+  dots : true,
+  responsive: [
+    {
+      breakpoint: 960,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '40px',
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: '0px',
+        slidesToShow: 1
+      }
+    }
+  ]
+});
