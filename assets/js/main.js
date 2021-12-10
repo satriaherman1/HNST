f42dde73d110eaa20c766c8b3bdd60853421916c
satriaherman <muhammadsatriaherman@gmail.com>
2021-12-10 21:23:49 +0700

    fix slider

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,26 +47,49 @@ const textList = ['Chess ♟️' , 'Econ 101 📊', 'Photography 📷', 'Guitar 
                   'Writing ✍️' ,'Speed Reading 📚', 'Personal Finance 💸' , 'Software 🧑‍💻',
                  'Piano 🎹', 'Français 🇫🇷' , 'Drawing 🎨', 'Copyright Law ⚖️' , 'Poker 🃏',
                  'Filmmaking 🎬']
+const slideContainer = document.querySelector('.slide-container .slider')
+textList.map((text) => {
+  slideContainer.innerHTML += `<h1 class="color-primary w-fit mx-auto finance-personal mt-2">${text}</h1>`
+})
+
 let indexTextList = 0;
 
 const changeTextListUp = () => {
-  el = document.querySelector('.finance-personal')
-  el.classList.remove('downAnimation')
-  el.classList.add('upAnimation')
-  setTimeout(() => changeTextListDown(), 1000)
-}
-const changeTextListDown = () => {
-  el.classList.remove('upAnimation')
-  el.classList.add('downAnimation')
-  el = document.querySelector('.finance-personal')
-  el.textContent = textList[indexTextList];
-  if(indexTextList >= textList.length){
-    indexTextList = 0;
+  el = [...document.querySelectorAll('.finance-personal')]
+  const slider = document.querySelector('.slider')
+  if(indexTextList >= textList.length - 1){
+      textList.map((text) => {
+        slideContainer.innerHTML += `<h1 class="color-primary w-fit mx-auto finance-personal mt-2">${text}</h1>`
+      })
+      indexTextList = 0
+    }
+  else{
+    indexTextList += 1;
+  }
+  const sliderStyle = getComputedStyle(slider)
+  let marginTop
+  if(window.innerWidth > 768){
+    marginTop = (82 * 2 + 21)
   }
   else{
-    indexTextList += 1
+    marginTop = (82 * 2 + 24)
   }
+  slider.style.marginTop = parseFloat(sliderStyle.getPropertyValue('margin-top')) - marginTop + "px"
+  
 }
 
-setInterval(changeTextListUp, 4000)
+// const changeTextListDown = () => {
+//   el.classList.remove('upAnimation')
+//   el.classList.add('downAnimation')
+//   el = document.querySelector('.finance-personal')
+//   el.textContent = textList[indexTextList];
+//   if(indexTextList >= textList.length){
+//     indexTextList = 0;
+//   }
+//   else{
+//     indexTextList += 1
+//   }
+// }
+
+setInterval(changeTextListUp, 2000)
 
