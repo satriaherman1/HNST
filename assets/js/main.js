98c98794f6a5c3969e3f34c66daf1778f983bd72
satriaherman <muhammadsatriaherman@gmail.com>
2021-12-16 23:11:48 +0700

    remove unused code

--- assets/js/main.js
+++ assets/js/main.js
@@ -43,48 +43,6 @@ const playVideo = (el) => {
   
 }
 
-// const textList = ['Chess ♟️' , 'Econ 101 📊', 'Photography 📷', 'Guitar 🎸' , 'YouTube ▶️',
-//                   'Writing ✍️' ,'Speed Reading 📚', 'Personal Finance 💸' , 'Software 🧑‍💻',
-//                  'Piano 🎹', 'Français 🇫🇷' , 'Drawing 🎨', 'Copyright Law ⚖️' , 'Poker 🃏',
-//                  'Filmmaking 🎬']
-// const slideContainer = document.querySelector('.slide-container .slider')
-// textList.map((text) => {
-//   slideContainer.innerHTML += `<h1 class="color-primary w-fit mx-auto finance-personal ">${text}</h1>`
-// })
-
-// let indexTextList = 0;
-
-// const changeTextListUp = () => {
-//   el = [...document.querySelectorAll('.finance-personal')]
-//   const slider = document.querySelector('.slider')
-//   let marginTop
-//   if(window.innerWidth > 768){
-//     marginTop = (82 * 2 + 20.75)
-//   }
-//   else{
-//     marginTop = (44 * 2 + 5)
-//   }
-//   if(indexTextList >= textList.length - 1){
-//     if(window.innerWidth > 768){
-//       marginTop += 0.3
-//     }
-//       textList.map((text) => {
-//         slideContainer.innerHTML += `<h1 class="color-primary w-fit mx-auto finance-personal ">${text}</h1>`
-//       })
-//       indexTextList = 0
-//     }
-//   else{
-//     indexTextList += 1;
-//   }
-//   const sliderStyle = getComputedStyle(slider)
-  
-//   slider.style.marginTop = parseFloat(sliderStyle.getPropertyValue('margin-top')) - marginTop + "px"
-  
-// }
-
-// setInterval(changeTextListUp, 2000)
-
-
 
 const maxCharChange =  (el) => {
   if(el.length.value < 300){
@@ -96,15 +54,10 @@ const confirmSubmit = () => {
   Swal.fire({
     title: 'Do you want to submit this form?',
     showDenyButton: true,
-    // showCancelButton: true,
     confirmButtonText: 'Submit',
     denyButtonText: `Not Yet`,
   }).then(result => {
     if(result.isConfirmed){
-      // $('#smallForm').removeClass('.show')
-      // $('#smallForm').css({
-      //   'display' : 'none'
-      // })
       const modal = document.querySelectorAll('.modal');
       const modalBackdrop = document.querySelectorAll('.modal-backdrop');
       modal.forEach((m) => {
